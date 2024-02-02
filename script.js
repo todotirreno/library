d730a668e3417ad6c0eeb9f65e1491a018bb5cba
todotirreno <16512686+todotirreno@users.noreply.github.com>
2024-02-02 18:11:45 -0300

    Revert "Fix naming"
    This reverts commit 865d83f5866d7c6a2a0fd1594d188dccc58b2a08.

--- script.js
+++ script.js
@@ -18,32 +18,20 @@ class Book {
 
 let library = []
 
-const addToLibrary = (newBook) => {
+function addToLibrary(newBook) {
   if (library.some((book) => book.title === newBook.title)) return false
   library.push(newBook)
-  saveLocal()
+  save()
   return true
 }
 
-const removeFromLibrary = (bookTitle) => {
+function removeFromLibrary(bookTitle) {
   library = library.filter((book) => book.title !== bookTitle)
-  saveLocal()
+  save()
 }
 
-const getBook = (bookTitle) => {
+function getBook(bookTitle) {
   return library.find((book) => book.title === bookTitle)
-}
-
-// LOCAL STORAGE
-
-const saveLocal = () => {
-  localStorage.setItem('myLibrary', JSON.stringify(library))
-}
-
-const restoreLocal = () => {
-  library = JSON.parse(localStorage.getItem('myLibrary'))
-  if (library === null) library = []
-  updateBooksGrid()
 }
 
 // ADD BOOK POPUP
@@ -59,20 +47,20 @@ window.onkeydown = (e) => {
 }
 
 function openAddBookPopup() {
-  addBookForm.reset()
-  addBookPopup.classList.add('active')
-  overlay.classList.add('active')
+  form.reset()
+  addBookPopup.classList.add('popup--active')
+  overlay.classList.add('overlay--active')
 }
 
 function closeAddBookPopup() {
-  addBookPopup.classList.remove('active')
-  overlay.classList.remove('active')
+  addBookPopup.classList.remove('popup--active')
+  overlay.classList.remove('overlay--active')
 }
 
 // FORM
 
-const addBookForm = document.getElementById('addBookForm')
-addBookForm.onsubmit = addBook
+const form = document.getElementById('popupForm')
+form.onsubmit = addBook
 
 function addBook(e) {
   e.preventDefault()
@@ -88,7 +76,7 @@ function getBookFromInput() {
   const title = `"${document.getElementById('title').value}"`
   const author = document.getElementById('author').value
   const pages = document.getElementById('pages').value
-  const isRead = document.getElementById('isRead').checked
+  const isRead = document.getElementById('is-read').checked
   return new Book(title, author, pages, isRead)
 }
 
@@ -105,15 +93,15 @@ function toggleRead(e) {
   if (e.target.innerHTML === 'Read') {
     getBook(e.target.parentNode.firstChild.innerHTML).isRead = false
     e.target.innerHTML = 'Not read'
-    e.target.classList.remove('btn-light-green')
-    e.target.classList.add('btn-light-red')
-    saveLocal()
+    e.target.classList.remove('btn--light-green')
+    e.target.classList.add('btn--light-red')
+    save()
   } else {
     getBook(e.target.parentNode.firstChild.innerHTML).isRead = true
     e.target.innerHTML = 'Read'
-    e.target.classList.remove('btn-light-red')
-    e.target.classList.add('btn-light-green')
-    saveLocal()
+    e.target.classList.remove('btn--light-red')
+    e.target.classList.add('btn--light-green')
+    save()
   }
 }
 
@@ -136,13 +124,13 @@ function createBookCard(book) {
   const readBtn = document.createElement('button')
   const removeBtn = document.createElement('button')
 
-  bookCard.classList.add('book-card')
-  title.classList.add('text')
-  author.classList.add('text')
-  pages.classList.add('text')
+  bookCard.classList.add('books-grid__book-card')
+  title.classList.add('book-card__text')
+  author.classList.add('book-card__text')
+  pages.classList.add('book-card__text')
   readBtn.classList.add('btn')
   removeBtn.classList.add('btn')
-  removeBtn.classList.add('btn-red')
+  removeBtn.classList.add('btn--red')
   readBtn.onclick = toggleRead
   removeBtn.onclick = removeBook
 
@@ -153,10 +141,10 @@ function createBookCard(book) {
   readBtn.style.width = '120px'
   if (book.isRead) {
     readBtn.textContent = 'Read'
-    readBtn.classList.add('btn-light-green')
+    readBtn.classList.add('btn--light-green')
   } else {
     readBtn.textContent = 'Not read'
-    readBtn.classList.add('btn-light-red')
+    readBtn.classList.add('btn--light-red')
   }
 
   bookCard.appendChild(title)
@@ -167,4 +155,74 @@ function createBookCard(book) {
   booksGrid.appendChild(bookCard)
 }
 
-restoreLocal()
+// AUTH
+
+const auth = firebase.auth()
+const whenSignedIn = document.getElementById('whenSignedIn')
+const whenSignedOut = document.getElementById('whenSignedOut')
+const loadingRing = document.getElementById('loadingRing')
+const signInBtn = document.getElementById('signInBtn')
+const signOutBtn = document.getElementById('signOutBtn')
+
+signInBtn.onclick = signIn
+signOutBtn.onclick = signOut
+
+function signIn() {
+  const provider = new firebase.auth.GoogleAuthProvider()
+  auth.signInWithPopup(provider)
+}
+
+function signOut() {
+  auth.signOut()
+}
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    loadingRing.classList.add('disable')
+    whenSignedOut.classList.remove('active')
+    whenSignedIn.classList.add('active')
+  } else {
+    loadingRing.classList.add('disable')
+    whenSignedIn.classList.remove('active')
+    whenSignedOut.classList.add('active')
+  }
+  restore()
+})
+
+// STORAGE
+
+function save() {
+  if (auth.currentUser) {
+    saveFirebase()
+  } else {
+    saveLocal()
+  }
+}
+
+function restore() {
+  if (auth.currentUser) {
+    restoreFirebase()
+  } else {
+    restoreLocal()
+  }
+}
+
+// FIRESTORE
+
+const db = firebase.firestore()
+
+function saveFirebase() {}
+
+function restoreFirebase() {}
+
+// LOCAL STORAGE
+
+function saveLocal() {
+  localStorage.setItem('myLibrary', JSON.stringify(library))
+}
+
+function restoreLocal() {
+  library = JSON.parse(localStorage.getItem('myLibrary'))
+  if (library === null) library = []
+  updateBooksGrid()
+}
