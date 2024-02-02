0c9ada592401e6a467cc88d64a9698dc3d968942
todotirreno <16512686+todotirreno@users.noreply.github.com>
2024-02-02 18:11:43 -0300

    Revert "Add firebase auth & firestore"
    This reverts commit 9f98c89091a318c4f37487d2bf2dcdf91deb6f3a.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// TODO - account modal, removing books, updating read
+// BOOK
 
 class Book {
   constructor(
     title = 'Unknown',
     author = 'Unknown',
     pages = '0',
-    isRead = false
+    isRead = 'false'
   ) {
     this.title = title
     this.author = author
@@ -14,65 +14,69 @@ class Book {
   }
 }
 
-class Library {
-  constructor() {
-    this.books = []
-  }
+// LIBRARY
 
-  addBook(newBook) {
-    if (this.books.some((book) => book.title === newBook.title)) return false
-    this.books.push(newBook)
-    return true
-  }
+let library = []
 
-  removeBook(title) {
-    this.books = this.books.filter((book) => book.title !== title)
-  }
-
-  getBook(title) {
-    return this.books.find((book) => book.title === title)
-  }
+const addToLibrary = (newBook) => {
+  if (library.some((book) => book.title === newBook.title)) return false
+  library.push(newBook)
+  saveLocal()
+  return true
 }
 
-const library = new Library()
+const removeFromLibrary = (bookTitle) => {
+  library = library.filter((book) => book.title !== bookTitle)
+  saveLocal()
+}
 
-//* UI
+const getBook = (bookTitle) => {
+  return library.find((book) => book.title === bookTitle)
+}
+
+// LOCAL STORAGE
+
+const saveLocal = () => {
+  localStorage.setItem('myLibrary', JSON.stringify(library))
+}
+
+const restoreLocal = () => {
+  library = JSON.parse(localStorage.getItem('myLibrary'))
+  if (library === null) library = []
+  updateBooksGrid()
+}
+
+// ADD BOOK POPUP
 
 const addBookBtn = document.getElementById('addBookBtn')
 const addBookPopup = document.getElementById('addBookPopup')
 const overlay = document.getElementById('overlay')
-const addBookForm = document.getElementById('addBookForm')
-const booksGrid = document.getElementById('booksGrid')
 
-const openAddBookPopup = () => {
+addBookBtn.onclick = openAddBookPopup
+overlay.onclick = closeAddBookPopup
+window.onkeydown = (e) => {
+  if (e.key === 'Escape') closeAddBookPopup()
+}
+
+function openAddBookPopup() {
   addBookForm.reset()
   addBookPopup.classList.add('active')
   overlay.classList.add('active')
 }
 
-const closeAddBookPopup = () => {
+function closeAddBookPopup() {
   addBookPopup.classList.remove('active')
   overlay.classList.remove('active')
 }
 
-const getBookFromInput = () => {
-  const title = `"${document.getElementById('title').value}"`
-  const author = document.getElementById('author').value
-  const pages = document.getElementById('pages').value
-  const isRead = document.getElementById('isRead').checked
-  return new Book(title, author, pages, isRead)
-}
+// FORM
 
-const addBook = (e) => {
+const addBookForm = document.getElementById('addBookForm')
+addBookForm.onsubmit = addBook
+
+function addBook(e) {
   e.preventDefault()
-  const newBook = getBookFromInput()
-
-  if (library.addBook(newBook)) {
-    if (auth.currentUser) {
-      db.collection('books').add(bookToJSON(newBook))
-    } else {
-      saveLocal()
-    }
+  if (addToLibrary(getBookFromInput())) {
     updateBooksGrid()
     closeAddBookPopup()
   } else {
@@ -80,21 +84,32 @@ const addBook = (e) => {
   }
 }
 
-const removeBook = (e) => {
+function getBookFromInput() {
+  const title = `"${document.getElementById('title').value}"`
+  const author = document.getElementById('author').value
+  const pages = document.getElementById('pages').value
+  const isRead = document.getElementById('isRead').checked
+  return new Book(title, author, pages, isRead)
+}
+
+// BOOKS GRID
+
+const booksGrid = document.getElementById('booksGrid')
+
+function removeBook(e) {
   removeFromLibrary(e.target.parentNode.firstChild.innerHTML)
   e.target.parentNode.parentNode.removeChild(e.target.parentNode)
 }
 
-const toggleRead = (e) => {
-  book = library.getBook(e.target.parentNode.firstChild.innerHTML)
-  if (book.isRead) {
-    book.isRead = false
+function toggleRead(e) {
+  if (e.target.innerHTML === 'Read') {
+    getBook(e.target.parentNode.firstChild.innerHTML).isRead = false
     e.target.innerHTML = 'Not read'
     e.target.classList.remove('btn-light-green')
     e.target.classList.add('btn-light-red')
     saveLocal()
   } else {
-    book.isRead = true
+    getBook(e.target.parentNode.firstChild.innerHTML).isRead = true
     e.target.innerHTML = 'Read'
     e.target.classList.remove('btn-light-red')
     e.target.classList.add('btn-light-green')
@@ -102,18 +117,18 @@ const toggleRead = (e) => {
   }
 }
 
-const updateBooksGrid = () => {
-  resetBooksGrid()
-  for (let book of library.books) {
-    createBookCard(book)
+function updateBooksGrid() {
+  resetGrid()
+  for (let element of library) {
+    createBookCard(element)
   }
 }
 
-const resetBooksGrid = () => {
+function resetGrid() {
   booksGrid.innerHTML = ''
 }
 
-const createBookCard = (book) => {
+function createBookCard(book) {
   const bookCard = document.createElement('div')
   const title = document.createElement('h3')
   const author = document.createElement('h3')
@@ -152,97 +167,4 @@ const createBookCard = (book) => {
   booksGrid.appendChild(bookCard)
 }
 
-addBookBtn.onclick = openAddBookPopup
-overlay.onclick = closeAddBookPopup
-addBookForm.onsubmit = addBook
-window.onkeydown = (e) => {
-  if (e.key === 'Escape') closeAddBookPopup()
-}
-
-//* LOCAL STORAGE
-
-const saveLocal = () => {
-  localStorage.setItem('library', JSON.stringify(library.books))
-}
-
-const restoreLocal = () => {
-  library.books = JSON.parse(localStorage.getItem('library'))
-  console.log()
-  if (library.books === null) library.books = []
-}
-
-//* FIREBASE
-
-const auth = firebase.auth()
-const db = firebase.firestore()
-const logInBtn = document.getElementById('logInBtn')
-const logOutBtn = document.getElementById('logOutBtn')
-const loggedIn = document.getElementById('loggedIn')
-const loggedOut = document.getElementById('loggedOut')
-const loadingRing = document.getElementById('loadingRing')
-
-auth.onAuthStateChanged(async (user) => {
-  if (user) {
-    await db.collection('books').onSnapshot((snapshot) => {
-      library.books = docsToBooks(snapshot.docs)
-      updateBooksGrid()
-    })
-  } else {
-    restoreLocal()
-    updateBooksGrid()
-  }
-  setupNavbar(user)
-})
-
-logInBtn.onclick = () => {
-  const provider = new firebase.auth.GoogleAuthProvider()
-  auth.signInWithPopup(provider)
-}
-
-logOutBtn.onclick = () => {
-  auth.signOut()
-}
-
-const setupNavbar = (user) => {
-  if (user) {
-    loggedIn.classList.add('active')
-    loggedOut.classList.remove('active')
-    loadingRing.classList.remove('active')
-  } else {
-    loggedIn.classList.remove('active')
-    loggedOut.classList.add('active')
-    loadingRing.classList.remove('active')
-  }
-}
-
-// * UTILS
-
-const docsToBooks = (docs) => {
-  return docs.map((doc) => {
-    return new Book(
-      doc.data().title,
-      doc.data().author,
-      doc.data().pages,
-      doc.data().isRead
-    )
-  })
-}
-
-const JSONToBooks = (booksJSON) => {
-  return booksJSON.map(
-    (book) => new Book(book.title, book.author, book.pages, book.isRead)
-  )
-}
-
-const booksToJSON = (books) => {
-  return books.map((book) => bookToJSON(book))
-}
-
-const bookToJSON = (book) => {
-  return {
-    title: book.title,
-    author: book.author,
-    pages: book.pages,
-    isRead: book.isRead,
-  }
-}
+restoreLocal()
